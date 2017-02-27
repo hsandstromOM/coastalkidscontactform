3af4469c810f4c40039191f82cd025f10dadd5f0
Obviouslee <hosea@obviouslee.com>
2017-02-27 15:41:14 -0500

    Redirect with validation

--- www/js/validate.js
+++ www/js/validate.js
@@ -100,22 +100,22 @@ $('#contact_form').bootstrapValidator({
                }
            }
        })
-       .on('success.form.bv', function(e) {
-           $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
-
-
-           // Prevent form submission
-          //  e.preventDefault();
-
-      //      // Get the form instance
-      //      var $form = $(e.target);
-       //
-      //      // Get the BootstrapValidator instance
-      //      var bv = $form.data('bootstrapValidator');
-       //
-      //      // Use Ajax to submit form data
-      //      $.post($form.attr('action'), $form.serialize(), function(result) {
-      //          console.log(result);
-      //      }, 'json');
-       });
+      //  .on('success.form.bv', function(e) {
+      //      $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+      //
+      //
+      //      // Prevent form submission
+      //     //  e.preventDefault();
+      //
+      // //      // Get the form instance
+      // //      var $form = $(e.target);
+      //  //
+      // //      // Get the BootstrapValidator instance
+      // //      var bv = $form.data('bootstrapValidator');
+      //  //
+      // //      // Use Ajax to submit form data
+      // //      $.post($form.attr('action'), $form.serialize(), function(result) {
+      // //          console.log(result);
+      // //      }, 'json');
+      //  });
 });
